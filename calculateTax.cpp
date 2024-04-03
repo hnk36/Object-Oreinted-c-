73a439412aac9bc7ff9514e568ba43faed7f00ce
Henok Dagne <henokdagne19@gmail.com>
2024-04-03 16:49:26 +0300

    Add calculateTax function.

---
+++ calculateTax.cpp
@@ -0,0 +1,7 @@
+//
+// Created by hp on 4/3/2024.
+//
+
+double calculateTax(double income,double taxRate) {
+    return (income * taxRate);
+}
--- main.cpp
+++ main.cpp
@@ -111,7 +111,16 @@ void Hospital::mapDeallocation(){
         cout<<"Record are not deleted"<<endl;
     }
 }
+/*void Hospital::roomNUmber(){
+    cout<<"Enter patient card number: ";
+    getline(cin, cardNumber);
+    if(lionHospital.find(cardNumber)) {
+        char choice;
+        cout<<"block 1 room"
+        cout<<"Enter your choos: ";
+        cin>>choice;
 
+    }*/
 // Define the main function
 int main(){
     // Create an instance of Hospital
